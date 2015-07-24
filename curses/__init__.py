420283cffbdc9a79460ec5e8d604a98ade09e9a4
Vlad Seghete <vlad.seghete@gmail.com>
2015-07-24 13:48:11 -0500

    added travis yaml file

--- curses/__init__.py
+++ curses/__init__.py
@@ -1,9 +1,14 @@
 import random
 
+
 nouns = ['mom', 'mike', 'dean', 'grandma']
 adjectives = ['stupid', 'fat', 'great', 'married']
 pronouns = ['my', 'your', 'our']
 verbs = ['is', 'was', 'likes', 'brings']
 
+
 def spew():
-    print random.choice(pronouns), random.choice(nouns), random.choice(verbs), random.choice(adjectives)        
+    print "%s %s %s %s" % (random.choice(pronouns),
+                           random.choice(nouns),
+                           random.choice(verbs),
+                           random.choice(adjectives))
